67676085c544eb14b47129ff852ae0177abb5973
Keaton J. Burns <keaton.burns@gmail.com>
2021-05-03 21:36:59 -0400

    Add labels and hide buffers

--- src/memtowel.py
+++ src/memtowel.py
@@ -15,11 +15,14 @@ class MemoryTowel:
         self.comm_size = comm.size
         self.filename = filename
         self.process = psutil.Process(os.getpid())
-        self.sendbuf = np.zeros(1, dtype=int)
+        self.write_number = 0
+        self._sendbuf = np.zeros(1, dtype=int)
         if self.comm_rank == 0:
-            self.recvbuf = np.zeros([self.comm_size, 1], dtype=int)
+            self._recvbuf = np.zeros([self.comm_size, 1], dtype=int)
+            self._recvflat = self._recvbuf.ravel()
         else:
-            self.recvbuf = None
+            self._recvbuf = None
+            self._recvflat = None
 
     def process_memory(self):
         """Measure memory usage by current process."""
@@ -27,38 +30,44 @@ class MemoryTowel:
 
     def comm_memory(self):
         """Gather memory usage by each process."""
-        self.sendbuf[0] = self.process_memory()
-        self.comm.Gather(self.sendbuf, self.recvbuf, root=0)
-        return self.recvbuf
+        if self.comm_size == 1:
+            return np.array([self.process_memory()], dtype=int)
+        else:
+            self._sendbuf[0] = self.process_memory()
+            self.comm.Gather(self._sendbuf, self._recvbuf, root=0)
+            return self._recvflat
 
-    def print_comm_memory(self):
+    def print_comm_memory(self, label=None):
         """Print comm memory."""
         comm_memory = self.comm_memory()
         if self.comm_rank == 0:
-            print(comm_memory.ravel())
+            if label is None:
+                print(comm_memory)
+            else:
+                print(f"{label} {comm_memory}")
 
-    def write_comm_memory(self, reset=False):
+    def write_comm_memory(self, label=None):
         """Save comm memory."""
         comm_memory = self.comm_memory()
         if self.comm_rank == 0:
-            if reset:
-                mode = "wb"
+            if label is None:
+                label = self.write_number
+            if self.write_number == 0:
+                mode = "w"
             else:
-                mode = "ab"
+                mode = "a"
             with open(self.filename, mode) as file:
-                np.savetxt(file, comm_memory.T, fmt='%i')
+                file.write(f"{label} {comm_memory}\n")
+        self.write_number += 1
 
 
 if __name__ == '__main__':
     # Test the towel
     memtowel = MemoryTowel()
     a = np.random.random((512,512,512))
-    memtowel.print_comm_memory()
-    memtowel.write_comm_memory(reset=True)
+    memtowel.print_comm_memory(label='one array ')
     b = np.random.random((512,512,512))
-    memtowel.print_comm_memory()
-    memtowel.write_comm_memory()
+    memtowel.print_comm_memory(label='two arrays')
     del b
-    memtowel.print_comm_memory()
-    memtowel.write_comm_memory()
+    memtowel.print_comm_memory(label='one array ')
 
